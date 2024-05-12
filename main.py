687352b7fe14a54580f2c06f4918a30b0dc9f58b
Diana <diana.castillo7676@alumnos.udg.mx>
2024-05-11 21:36:40 -0600

    Solo cursos gratis y activos

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # pip install discord
+# pip install python-dotenv
 # pip install requests
 # pip install beautifulsoup4
 
@@ -6,6 +7,7 @@ import os
 import requests
 import asyncio
 from discord import Client
+from dotenv import load_dotenv
 from discord_bot import DiscordBot
 from bs4 import BeautifulSoup
 
@@ -27,37 +29,48 @@ class WebCrawler(DiscordBot):
                # Realiza la solicitud HTTP
                response = requests.get(url)
                
-               if response.status_code == 200:
-                    soup = BeautifulSoup(response.content, 'html.parser')
-                    course_elements = soup.find_all('a', class_='c-card')
-
-                    if not course_elements:
-                         break
-
-                    for element in course_elements:
-                         title = element.find('h2').text.strip()
-                         if self.keyword.lower() in title.lower():
-                              link = element.get('href')
-                              if link != self.last_course:
-                                   courses.append({
-                                        'title': title,
-                                        'link': link
-                                   })
-                                   if self.last_course == None:
-                                        search = False
-                                        break
-                              else:
-                                   search = False
-                                   break
-                    if search:
-                         page += 1
-                         asyncio.sleep(1)
-               
-               else:
+               if response.status_code != 200:
                     print('La solicitud a la página ' + str(page) + ' no fue exitosa')
                     break
-          
-          if len(courses) and self.last_course != courses[0]['link']:
+
+               soup = BeautifulSoup(response.content, 'html.parser')
+
+               divs_courses = soup.find_all('div', class_='w-screen') #
+               if len(divs_courses) >= 2:
+                    div_free_courses = divs_courses[1]
+               else:
+                    break #
+
+               expired_courses = div_free_courses.find_all('span', string='Expired') #
+               if len(expired_courses) == 20:
+                    break #
+
+               course_elements = div_free_courses.find_all('a', class_='c-card') #
+               if not course_elements:
+                    break
+
+               for element in course_elements:
+                    expired = element.find('span', string='Expired') #
+                    title = element.find('h2').text.strip()
+                    if not expired and self.keyword.lower() in title.lower(): #
+                         link = element.get('href')
+                         if link != self.last_course:
+                              courses.append({
+                                   'title': title,
+                                   'link': link
+                              })
+                              if self.last_course == None:
+                                   search = False
+                                   break
+                         else:
+                              search = False
+                              break
+               
+               if search:
+                    page += 1
+                    asyncio.sleep(1)
+               
+          if len(courses):
                self.last_course = courses[0]['link']
 
           return courses
@@ -66,6 +79,7 @@ class WebCrawler(DiscordBot):
 if __name__ == '__main__':
 
      # Carga las variables de entorno
+     load_dotenv()
      TOKEN: str = os.getenv('DISCORD_TOKEN')
      CHANNEL_ID: int = int(os.getenv('CHANNEL_ID'))
 
